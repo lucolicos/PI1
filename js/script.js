6befdb7996e0213ed5fea7ce1364edb6028564a0
lucolicos <148923202+lucolicos@users.noreply.github.com>
2024-05-26 19:10:30 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -39,23 +39,23 @@ document.getElementById('fechar').addEventListener('click', function() {
 });
 
 const bancoDados = [
-    { nome: 'Dr Alexander Flemming', crm: '999999', status: 'Ativo', especialidade: 'Clínico Geral', dataCadastro: '06/09/1891', login: 'alexander_flemming' },
-    { nome: 'Dr Sigmund Freud', crm: '888888', status: 'Ativo', especialidade: 'Neurologia', dataCadastro: '06/05/1856', login: 'sigmund_freud' },
-    { nome: 'Dr Edward Jenner', crm: '777777', status: 'Ativo', especialidade: 'Imunologia', dataCadastro: '17/05/1749', login: 'edward_jenner' },
-    { nome: 'Dr Joseph Lister', crm: '666666', status: 'Ativo', especialidade: 'Cirurgia', dataCadastro: '05/04/1827', login: 'joseph_lister' },
-    { nome: 'Dr Andreas Vesalius', crm: '555555', status: 'Ativo', especialidade: 'Anatomia', dataCadastro: '31/12/1514', login: 'andreas_vesalius' },
-    { nome: 'Dr William Osler', crm: '444444', status: 'Ativo', especialidade: 'Medicina Interna', dataCadastro: '12/07/1849', login: 'william_osler' },
-    { nome: 'Dr Ignaz Semmelweis', crm: '333333', status: 'Ativo', especialidade: 'Obstetrícia', dataCadastro: '01/07/1818', login: 'ignaz_semmelweis' },
-    { nome: 'Dr Hippocrates', crm: '222222', status: 'Ativo', especialidade: 'Medicina', dataCadastro: '01/01/460 AC', login: 'hippocrates' },
-    { nome: 'Dr Paracelsus', crm: '111111', status: 'Ativo', especialidade: 'Toxicologia', dataCadastro: '11/11/1493', login: 'paracelsus' },
-    { nome: 'Dr Avicenna', crm: '000000', status: 'Ativo', especialidade: 'Filosofia Médica', dataCadastro: '22/08/980', login: 'avicenna' }
+    { nome: 'Alexander Flemming', crm: '999999', status: 'Ativo', especialidade: 'Clínico Geral', dataCadastro: '06/09/1891', login: 'alexander_flemming' },
+    { nome: 'Sigmund Freud', crm: '888888', status: 'Ativo', especialidade: 'Neurologia', dataCadastro: '06/05/1856', login: 'sigmund_freud' },
+    { nome: 'Edward Jenner', crm: '777777', status: 'Ativo', especialidade: 'Imunologia', dataCadastro: '17/05/1749', login: 'edward_jenner' },
+    { nome: 'Joseph Lister', crm: '666666', status: 'Ativo', especialidade: 'Cirurgia', dataCadastro: '05/04/1827', login: 'joseph_lister' },
+    { nome: 'Andreas Vesalius', crm: '555555', status: 'Ativo', especialidade: 'Anatomia', dataCadastro: '31/12/1514', login: 'andreas_vesalius' },
+    { nome: 'William Osler', crm: '444444', status: 'Ativo', especialidade: 'Medicina Interna', dataCadastro: '12/07/1849', login: 'william_osler' },
+    { nome: 'Ignaz Semmelweis', crm: '333333', status: 'Ativo', especialidade: 'Obstetrícia', dataCadastro: '01/07/1818', login: 'ignaz_semmelweis' },
+    { nome: 'Hippocrates', crm: '222222', status: 'Ativo', especialidade: 'Medicina', dataCadastro: '01/01/460 AC', login: 'hippocrates' },
+    { nome: 'Paracelsus', crm: '111111', status: 'Ativo', especialidade: 'Toxicologia', dataCadastro: '11/11/1493', login: 'paracelsus' },
+    { nome: 'Avicenna', crm: '000000', status: 'Ativo', especialidade: 'Filosofia Médica', dataCadastro: '22/08/980', login: 'avicenna' }
 ];
 
 function obterDados(pesquisaPor, valor) {
+    valor = valor.toLowerCase();
     if (pesquisaPor === 'crm') {
-        return bancoDados.find(dados => dados.crm === valor);
+        return bancoDados.find(dados => dados.crm.toLowerCase() === valor);
     } else if (pesquisaPor === 'nome') {
-        valor = valor.toLowerCase();
         return bancoDados.find(dados => dados.nome.toLowerCase().includes(valor));
     }
     return null;
